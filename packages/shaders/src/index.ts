0e32411c43542277e13bbd3833004d820ef5c3ba
Maxime <heckelmaxime@gmail.com>
2024-11-12 22:14:10 -0500

    feat: Add mesh gradient shader

--- packages/shaders/src/index.ts
+++ packages/shaders/src/index.ts
@@ -1,12 +1,18 @@
 /** The core Shader Mounting class. Pass it a canvas element and a fragment shader to get started. */
-export { ShaderMount } from './shader-mount';
+export { ShaderMount } from "./shader-mount";
 
 // ----- Fragment shaders ----- //
 /** An example fragment shader that renders a grainy texture over top of blobby animated clouds */
 export {
   grainCloudsFragmentShader,
   type GrainCloudsUniforms,
-} from './shaders/grain-clouds';
+} from "./shaders/grain-clouds";
+
+/** A shader that renders a mesh gradient with a rotating noise pattern and several layers of fractal noise */
+export {
+  meshGradientFragmentShader,
+  type MeshGradientUniforms,
+} from "./shaders/mesh-gradient";
 
 // ----- Uniform conversion utils ----- //
-export { getShaderColorFromString } from './shader-mount';
+export { getShaderColorFromString } from "./shader-mount";
