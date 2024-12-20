c0860dc0a66820706550e81eb2d74c9f2a4f0e25
Ksenia Kondrashova <kondrashova.ksenya@gmail.com>
2024-12-21 01:08:33 +0400

    metaballs first version

---
+++ example/src/shaders/metaballs-example.tsx
@@ -0,0 +1,58 @@
+import { Metaballs, type MetaballsParams, metaballsPresets } from '@paper-design/shaders-react';
+import { useControls, button, folder } from 'leva';
+import { useEffect } from 'react';
+
+/**
+ * You can copy/paste this example to use Metaballs in your app
+ */
+const MetaballsExample = () => {
+  return (
+    <Metaballs
+      color1="#6a5496"
+      color2="#9b8ab8"
+      color3="#f5d03b"
+      scale={11}
+      speed={1}
+      visibilityRange={0}
+      dotSize={1}
+      style={{ position: 'fixed', width: '100%', height: '100%' }}
+    />
+  );
+};
+
+/**
+ * This example has controls added so you can play with settings in the example app
+ */
+
+const defaults = metaballsPresets[0].params;
+
+export const MetaballsWithControls = () => {
+  const [params, setParams] = useControls(() => {
+    const presets: MetaballsParams = Object.fromEntries(
+      metaballsPresets.map((preset) => [preset.name, button(() => setParams(preset.params))])
+    );
+    return {
+      Parameters: folder(
+        {
+          color1: { value: defaults.color1, order: 1 },
+          color2: { value: defaults.color2, order: 2 },
+          color3: { value: defaults.color3, order: 3 },
+          speed: { value: defaults.speed, order: 4, min: -2, max: 2 },
+          scale: { value: defaults.scale, order: 5, min: 1, max: 20 },
+          dotSize: { value: defaults.scale, order: 6, min: 0, max: 1 },
+          visibilityRange: { value: defaults.scale, order: 7, min: 0, max: 1 },
+        },
+        { order: 1 }
+      ),
+      Presets: folder(presets, { order: 2 }),
+    };
+  });
+
+  // Reset to defaults on mount, so that Leva doesn't show values from other
+  // shaders when navigating (if two shaders have a color1 param for example)
+  useEffect(() => {
+    setParams(defaults);
+  }, []);
+
+  return <Metaballs {...params} style={{ position: 'fixed', width: '100%', height: '100%' }} />;
+};
---
+++ packages/shaders-react/src/shaders/metaballs.tsx
@@ -0,0 +1,54 @@
+import { useMemo } from 'react';
+import { ShaderMount, type GlobalParams, type ShaderMountProps } from '../shader-mount';
+import { getShaderColorFromString, metaballsFragmentShader, type MetaballsUniforms } from '@paper-design/shaders';
+
+export type MetaballsParams = {
+  color1?: string;
+  color2?: string;
+  color3?: string;
+  scale?: number;
+  dotSize?: number;
+  visibilityRange?: number;
+} & GlobalParams;
+
+export type MetaballsProps = Omit<ShaderMountProps, 'fragmentShader'> & MetaballsParams;
+
+type MetaballsPreset = { name: string; params: Required<MetaballsParams> };
+
+export const defaultPreset: MetaballsPreset = {
+  name: 'Default',
+  params: {
+    // Note: Keep default colors in HSLA format so that our Leva controls show a transparency channel (rgba and hex8 do not work)
+    color1: 'hsla(350, 90%, 55%, 1)',
+    color2: 'hsla(350, 80%, 60%, 1)',
+    color3: 'hsla(20, 85%, 70%, 1)',
+    scale: 10,
+    speed: 1.2,
+    dotSize: 1,
+    visibilityRange: .55,
+  },
+} as const;
+
+export const metaballsPresets: MetaballsPreset[] = [defaultPreset];
+
+export const Metaballs = (props: MetaballsProps): JSX.Element => {
+  const uniforms: MetaballsUniforms = useMemo(() => {
+    return {
+      u_color1: getShaderColorFromString(props.color1, defaultPreset.params.color1),
+      u_color2: getShaderColorFromString(props.color2, defaultPreset.params.color2),
+      u_color3: getShaderColorFromString(props.color3, defaultPreset.params.color3),
+      u_scale: props.scale ?? defaultPreset.params.scale,
+      u_dotSize: props.dotSize ?? defaultPreset.params.dotSize,
+      u_visibilityRange: props.visibilityRange ?? defaultPreset.params.visibilityRange,
+    };
+  }, [
+    props.color1,
+    props.color2,
+    props.color3,
+    props.scale,
+    props.dotSize,
+    props.visibilityRange,
+  ]);
+
+  return <ShaderMount {...props} fragmentShader={metaballsFragmentShader} uniforms={uniforms} />;
+};
