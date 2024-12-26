41e525bf49dcd59d46123b9e630e7efa61925c00
verekia <522007+verekia@users.noreply.github.com>
2024-12-26 13:31:33 +0700

    Add title for shader pages, back button shadow and transition

--- docs/src/components/back-button.tsx
+++ docs/src/components/back-button.tsx
@@ -3,7 +3,7 @@ import { BackIcon } from '@/icons';
 export function BackButton({ className = '' }: { className?: string }) {
   return (
     <button
-      className={` ${className} fixed top-3 left-3 aspect-square rounded-full bg-white/80 hover:bg-white/90 z-10 p-2`}
+      className={` ${className} fixed top-3 left-3 aspect-square rounded-full bg-white/80 hover:bg-white z-10 p-2 shadow transition-all duration-300`}
     >
       <BackIcon className="size-6" />
     </button>
