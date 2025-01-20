fac8f97c80d219e7dd8ca914269b677175838b64
Ksenia Kondrashova <kondrashova.ksenya@gmail.com>
2025-01-20 16:25:19 +0400

    more on presets

--- packages/shaders-react/src/shaders/radial-swirl.tsx
+++ packages/shaders-react/src/shaders/radial-swirl.tsx
@@ -29,12 +29,12 @@ export const defaultPreset: RadialSwirlPreset = {
     colorFront: 'hsla(189, 63%, 30%, 1)',
     colorStripe1: 'hsla(60, 92%, 71%, 1)',
     colorStripe2: 'hsla(0, 0%, 0%, 1)',
-    density: .93,
-    speed: .3,
+    density: 0.93,
+    speed: 0.3,
     proportion: 0,
     stripe1: 1,
     stripe2: 0,
-    focus: .15,
+    focus: 0.15,
     noiseFreq: 0,
     noisePower: 0,
     seed: 0,
@@ -50,11 +50,11 @@ export const bubbleGumPreset: RadialSwirlPreset = {
     colorFront: 'hsla(160, 70%, 75%, 1)',
     colorStripe1: 'hsla(290, 80%, 70%, 1)',
     colorStripe2: 'hsla(200, 60%, 60%, 1)',
-    density: .75,
-    speed: .3,
-    proportion: .63,
-    stripe1: .72,
-    stripe2: .79,
+    density: 0.75,
+    speed: 0.3,
+    proportion: 0.63,
+    stripe1: 0.72,
+    stripe2: 0.79,
     focus: 0,
     noiseFreq: 0,
     noisePower: 0,
@@ -73,7 +73,7 @@ export const classicPreset: RadialSwirlPreset = {
     colorStripe2: 'hsla(0, 0%, 0%, 1)',
     density: 1.75,
     speed: 0,
-    proportion: .4,
+    proportion: 0.4,
     stripe1: 0,
     stripe2: 0,
     focus: 0,
@@ -92,20 +92,46 @@ export const swampPreset: RadialSwirlPreset = {
     colorFront: 'hsla(120, 80%, 35%, 1)',
     colorStripe1: 'hsla(122, 78%, 20%, 1)',
     colorStripe2: 'hsla(0, 0%, 0%, 1)',
-    density: .98,
-    speed: .3,
-    proportion: .5,
-    stripe1: .94,
-    stripe2: .75,
+    density: 0.98,
+    speed: 0.3,
+    proportion: 0.5,
+    stripe1: 0.94,
+    stripe2: 0.75,
     focus: 0,
-    noiseFreq: .75,
+    noiseFreq: 0.75,
     noisePower: 1.4,
     seed: 0,
   },
 } as const;
 
+export const funnelPreset: RadialSwirlPreset = {
+  name: 'Funnel',
+  params: {
+    // Note: Keep default colors in HSLA format so that our Leva controls show a transparency channel (rgba and hex8 do not work)
+    // And don't use decimal values or highlights won't work, because the values get rounded and highlights need an exact match.
+    colorBack: 'hsla(0, 0%, 100%, 1)',
+    colorFront: 'hsla(239, 60%, 37%, 1)',
+    colorStripe1: 'hsla(239, 60%, 37%, 1)',
+    colorStripe2: 'hsla(0, 0%, 0%, 1)',
+    density: 1.85,
+    speed: 0.55,
+    proportion: 0.45,
+    stripe1: 1,
+    stripe2: 0,
+    focus: 0.7,
+    noiseFreq: 0,
+    noisePower: 0,
+    seed: 0,
+  },
+} as const;
 
-export const radialSwirlPresets: RadialSwirlPreset[] = [defaultPreset, bubbleGumPreset, classicPreset, swampPreset];
+export const radialSwirlPresets: RadialSwirlPreset[] = [
+  defaultPreset,
+  bubbleGumPreset,
+  classicPreset,
+  swampPreset,
+  funnelPreset,
+];
 
 export const RadialSwirl = (props: RadialSwirlProps): JSX.Element => {
   const uniforms: RadialSwirlUniforms = useMemo(() => {
@@ -123,7 +149,20 @@ export const RadialSwirl = (props: RadialSwirlProps): JSX.Element => {
       u_noisePower: props.noisePower ?? defaultPreset.params.noisePower,
       u_seed: props.seed ?? defaultPreset.params.seed,
     };
-  }, [props.colorBack, props.colorFront, props.colorStripe1, props.colorStripe2, props.density, props.proportion, props.stripe1, props.stripe2, props.focus, props.noiseFreq, props.noisePower, props.seed]);
+  }, [
+    props.colorBack,
+    props.colorFront,
+    props.colorStripe1,
+    props.colorStripe2,
+    props.density,
+    props.proportion,
+    props.stripe1,
+    props.stripe2,
+    props.focus,
+    props.noiseFreq,
+    props.noisePower,
+    props.seed,
+  ]);
 
   return <ShaderMount {...props} fragmentShader={radialSwirlFragmentShader} uniforms={uniforms} />;
 };
