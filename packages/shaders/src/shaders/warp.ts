25b92083aa2190f7ba2f00eac539722f81f4698d
Ksenia Kondrashova <kondrashova.ksenya@gmail.com>
2025-01-29 11:05:51 +0100

    distortion noise replacement #3 (simplex)

--- packages/shaders/src/shaders/warp.ts
+++ packages/shaders/src/shaders/warp.ts
@@ -72,20 +72,33 @@ vec2 rotate(vec2 uv, float th) {
   return mat2(cos(th), sin(th), -sin(th), cos(th)) * uv;
 }
 
-float hash(vec2 p) {
-    p = 50.0 * fract(p * 0.3183099 + vec2(0.71, 0.113));
-    return fract(p.x * p.y * (p.x + p.y));
-}
 
-float improvedNoise(vec2 st) {
-    vec2 i = floor(st);
-    vec2 f = fract(st);
-    float a = hash(i);
-    float b = hash(i + vec2(1.0, 0.0));
-    float c = hash(i + vec2(0.0, 1.0));
-    float d = hash(i + vec2(1.0, 1.0));
-    vec2 u = f * f * (3.0 - 2.0 * f);
-    return mix(a, b, u.x) + (c - a) * u.y * (1.0 - u.x) + (d - b) * u.x * u.y;
+vec3 permute(vec3 x) { return mod(((x * 34.0) + 1.0) * x, 289.0); }
+float snoise(vec2 v) {
+  const vec4 C = vec4(0.211324865405187, 0.366025403784439,
+    -0.577350269189626, 0.024390243902439);
+  vec2 i = floor(v + dot(v, C.yy));
+  vec2 x0 = v - i + dot(i, C.xx);
+  vec2 i1;
+  i1 = (x0.x > x0.y) ? vec2(1.0, 0.0) : vec2(0.0, 1.0);
+  vec4 x12 = x0.xyxy + C.xxzz;
+  x12.xy -= i1;
+  i = mod(i, 289.0);
+  vec3 p = permute(permute(i.y + vec3(0.0, i1.y, 1.0))
+    + i.x + vec3(0.0, i1.x, 1.0));
+  vec3 m = max(0.5 - vec3(dot(x0, x0), dot(x12.xy, x12.xy),
+      dot(x12.zw, x12.zw)), 0.0);
+  m = m * m;
+  m = m * m;
+  vec3 x = 2.0 * fract(p * C.www) - 1.0;
+  vec3 h = abs(x) - 0.5;
+  vec3 ox = floor(x + 0.5);
+  vec3 a0 = x - ox;
+  m *= 1.79284291400159 - 0.85373472095314 * (a0 * a0 + h * h);
+  vec3 g;
+  g.x = a0.x * x0.x + h.x * x0.y;
+  g.yz = a0.yz * x12.xz + h.yz * x12.yw;
+  return 130.0 * dot(m, g);
 }
 
 vec4 blend_colors(vec4 c1, vec4 c2, vec4 c3, float mixer, float edgesWidth) {
@@ -118,8 +131,8 @@ void main() {
     uv /= u_pixelRatio;
     uv += .5;
         
-    float n1 = improvedNoise(uv * 1. + t);
-    float n2 = improvedNoise(uv * 2. - t);
+    float n1 = snoise(uv * .3 + .6 * t);
+    float n2 = snoise(uv * .7 - .5 * t);
     uv.x += 4. * u_distortion * n1;
     uv.y += 4. * u_distortion * n2;
 
