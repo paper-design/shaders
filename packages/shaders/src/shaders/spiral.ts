ffe074530e296e3e83e73832512f75fb1c68b62b
Ksenia Kondrashova <kondrashova.ksenya@gmail.com>
2025-02-06 16:43:08 +0000

    more on noise quality

--- packages/shaders/src/shaders/spiral.ts
+++ packages/shaders/src/shaders/spiral.ts
@@ -54,20 +54,33 @@ uniform vec2 u_resolution;
 
 out vec4 fragColor;
 
-float random(vec2 st) {
-  return fract(sin(dot(st.xy, vec2(12.9898, 78.233))) * 43758.5453123);
-}
-float noise(vec2 st) {
-  vec2 i = floor(st);
-  vec2 f = fract(st);
-  float a = random(i);
-  float b = random(i + vec2(1.0, 0.0));
-  float c = random(i + vec2(0.0, 1.0));
-  float d = random(i + vec2(1.0, 1.0));
-  vec2 u = f * f * (3.0 - 2.0 * f);
-  float x1 = mix(a, b, u.x);
-  float x2 = mix(c, d, u.x);
-  return mix(x1, x2, u.y);
+vec3 permute(vec3 x) { return mod(((x * 34.0) + 1.0) * x, 289.0); }
+
+float snoise(vec2 v) {
+  const vec4 C = vec4(0.211324865405187, 0.366025403784439,
+    -0.577350269189626, 0.024390243902439);
+  vec2 i = floor(v + dot(v, C.yy));
+  vec2 x0 = v - i + dot(i, C.xx);
+  vec2 i1;
+  i1 = (x0.x > x0.y) ? vec2(1.0, 0.0) : vec2(0.0, 1.0);
+  vec4 x12 = x0.xyxy + C.xxzz;
+  x12.xy -= i1;
+  i = mod(i, 289.0);
+  vec3 p = permute(permute(i.y + vec3(0.0, i1.y, 1.0))
+    + i.x + vec3(0.0, i1.x, 1.0));
+  vec3 m = max(0.5 - vec3(dot(x0, x0), dot(x12.xy, x12.xy),
+      dot(x12.zw, x12.zw)), 0.0);
+  m = m * m;
+  m = m * m;
+  vec3 x = 2.0 * fract(p * C.www) - 1.0;
+  vec3 h = abs(x) - 0.5;
+  vec3 ox = floor(x + 0.5);
+  vec3 a0 = x - ox;
+  m *= 1.79284291400159 - 0.85373472095314 * (a0 * a0 + h * h);
+  vec3 g;
+  g.x = a0.x * x0.x + h.x * x0.y;
+  g.yz = a0.yz * x12.xz + h.yz * x12.yw;
+  return 130.0 * dot(m, g);
 }
 
 void main() {
@@ -86,23 +99,24 @@ void main() {
 
   float l = length(uv);
   float angle = atan(uv.y, uv.x) - 2. * t;
-  float angle_norm = angle / TWO_PI;
+  float angle_norm = angle / TWO_PI;  
 
-  float offset = pow(l, 1. - u_spiralDensity) + angle_norm;  
+  angle_norm += .5 * u_noisePower * snoise(.5 * u_noiseFreq * uv);
+
+  float offset = pow(l, 1. - u_spiralDensity) + angle_norm;
   
   float stripe_map = fract(offset);
   stripe_map -= u_strokeTaper * l;
+  
+  stripe_map += u_noisePower * snoise(u_noiseFreq * uv);
 
   float shape = 2. * abs(stripe_map - .5);
   
   shape *= (1. + u_spiralDistortion * sin(4. * l - t) * cos(PI + l + t));
 
   shape *= (1. - u_strokeCap * max(u_spiralDistortion, u_blur) / l);
-  
+    
   float stroke_width = clamp(u_strokeWidth, fwidth(l), 1. - fwidth(l));
-  float n2 = noise(uv * .2 * u_noiseFreq - .5 * t);
-  n2 *= noise(uv * u_noiseFreq);
-  stroke_width += u_noisePower * n2;
 
   float edge_width = min(fwidth(l), fwidth(offset));
 
@@ -116,5 +130,6 @@ void main() {
   float opacity = mix(u_color1.a, u_color2.a, shape);
 
   fragColor = vec4(color, opacity);
+  // fragColor = vec4(vec3(n), 1.);
 }
 `;
