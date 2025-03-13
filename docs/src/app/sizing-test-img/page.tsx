91d26c622b58baef00a362bd879b1d50ae5b73c9
Ksenia Kondrashova <kondrashova.ksenya@gmail.com>
2025-03-13 16:52:56 +0100

    more on image texture legend fix x 2

--- docs/src/app/sizing-test-img/page.tsx
+++ docs/src/app/sizing-test-img/page.tsx
@@ -139,14 +139,14 @@ const MyTest = () => {
             <div style={{
                 position: 'fixed',
                 left: `10px`,
-                top: `10px`,
-            }}><span style={{color: 'red'}}>red</span>: image box
+                top: `40px`,
+            }}><span style={{color: 'red'}}>red</span>: effect size in px (effect size = 3 waves)
             </div>
             <div style={{
                 position: 'fixed',
                 left: `10px`,
-                top: `40px`,
-            }}><span style={{color: 'red'}}>green</span>: effect size in px (effect size = 3 waves)
+                top: `10px`,
+            }}><span style={{color: 'green'}}>green</span>: image box
             </div>
             <div
                 style={{
