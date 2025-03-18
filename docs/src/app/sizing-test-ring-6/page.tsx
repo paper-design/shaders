0006dcbb636dd2b46d544816276e46c28d5a65c3
Ksenia Kondrashova <kondrashova.ksenya@gmail.com>
2025-03-18 21:48:48 +0400

    demo 6

---
+++ docs/src/app/sizing-test-ring-6/page.tsx
@@ -0,0 +1,150 @@
+'use client';
+import { ShaderMount } from '@paper-design/shaders-react';
+import { useControls } from 'leva';
+
+const fragmentShader = `#version 300 es
+precision highp float;
+
+uniform float u_pixelRatio;
+uniform vec2 u_resolution;
+uniform float u_time;
+
+uniform float u_worldWidth;
+uniform float u_worldHeight;
+uniform float u_fit;
+
+out vec4 fragColor;
+
+#define TWO_PI 6.28318530718
+#define PI 3.14159265358979323846
+
+
+void main() {
+
+  vec2 uv = gl_FragCoord.xy / u_resolution.xy;
+  float ratio = u_resolution.x / u_resolution.y;
+  float worldRatio = u_worldWidth / u_worldHeight;
+
+  uv -= .5;
+
+  vec2 world = vec2(u_worldWidth, u_worldHeight) * u_pixelRatio;
+
+  float xRatio = u_resolution.x / world.x;
+  float yRatio = u_resolution.y / world.y;
+
+  uv *= vec2(xRatio, yRatio);
+
+
+  if (u_fit == 0.) {
+    if (ratio > 1.) {
+      uv.x *= worldRatio;
+      if (yRatio > 1.) {
+        uv /= yRatio;
+      }
+      if (xRatio < 1.) {
+        if (worldRatio < 1.) {
+          uv /= worldRatio;
+        }
+      } else {
+        if (world.y > u_resolution.x) {
+          uv /= worldRatio;
+          uv /= xRatio;
+        }
+      }
+    } else {
+      uv.y /= worldRatio;
+      if (xRatio > 1.) {
+        uv /= xRatio;
+      }
+      if (world.x > u_resolution.y) {
+        if (yRatio > 1.) {
+          uv *= worldRatio;
+          uv /= yRatio;
+        } else {
+          if (worldRatio > 1.) {
+            uv *= worldRatio;
+          }
+        }
+      }
+    }
+  } else {
+    if (ratio > 1.) {
+      uv.x *= worldRatio;
+      if (xRatio < 1.) {
+        if (worldRatio > 1.) {
+          uv /= worldRatio;
+        }
+      } else {
+        if (world.y < u_resolution.x) {
+          uv /= worldRatio;
+          uv /= xRatio;
+        }
+      }
+    } else {
+      uv.y /= worldRatio;
+      if (world.x > u_resolution.y) {
+        if (worldRatio < 1.) {
+          uv *= worldRatio;
+        }
+      } else {
+        uv *= worldRatio;
+        if (worldRatio > 1.) {
+          uv /= yRatio;
+        } else {
+          if (yRatio > 1.) {
+            uv /= yRatio;
+          }
+        }
+      }
+    }
+  }
+
+
+  float ring_shape = 2. - (smoothstep(.1, .3, length(uv + vec2(-.15, 0.))) + smoothstep(.1, .3, length(uv + vec2(.15, 0.))));
+  vec3 color = normalize(vec3(.4, .2, 1.)) * 2. * ring_shape;
+
+  color = mix(color, vec3(.4), 1. - step(uv.x, .5));
+  color = mix(color, vec3(.4), 1. - step(uv.y, .5));
+  color = mix(color, vec3(.4), step(uv.x, -.5));
+  color = mix(color, vec3(.4), step(uv.y, -.5));
+
+  fragColor = vec4(color, 1.);
+}
+`;
+
+const MyTest = () => {
+  const { width, height } = useControls('canvas', {
+    width: { value: 300, min: 10, max: 1000 },
+    height: { value: 700, min: 10, max: 1000 },
+  });
+
+  const { fit, worldWidth, worldHeight } = useControls('shader', {
+    fit: { value: 0, min: 0, max: 1, step: 1 },
+    worldWidth: { value: 600, min: 10, max: 1000 },
+    worldHeight: { value: 950, min: 10, max: 1000 },
+  });
+
+  return (
+    <>
+        <div
+          style={{
+            width: `${width}px`,
+            height: `${height}px`,
+            position: 'absolute',
+          }}
+        >
+          <ShaderMount
+            style={{ width: '100%', height: '100%' }}
+            fragmentShader={fragmentShader}
+            uniforms={{
+              u_worldWidth: worldWidth,
+              u_worldHeight: worldHeight,
+              u_fit: fit,
+            }}
+          />
+        </div>
+    </>
+  );
+};
+
+export default MyTest;
