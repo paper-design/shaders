776750468bc3efe79b12a317ff3d7a12a1b7efe5
Ksenia Kondrashova <kondrashova.ksenya@gmail.com>
2025-04-17 13:54:55 +0400

    fix registry

--- docs/registry/swirl-example.tsx
+++ docs/registry/swirl-example.tsx
@@ -2,6 +2,6 @@ import { Swirl, SwirlProps } from '@paper-design/shaders-react';
 
 export function SwirlExample(props: SwirlProps) {
   return (
-    <Swirl color1="#90e32b" color2="#2c8618" style={{ position: 'fixed', width: '100%', height: '100%' }} {...props} />
+    <Swirl style={{ position: 'fixed', width: '100%', height: '100%' }} {...props} />
   );
 }
