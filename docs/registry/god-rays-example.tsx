2ea01430216b3284255c7cb8d267a22f8e597715
Ksenia Kondrashova <kondrashova.ksenya@gmail.com>
2025-04-23 22:42:54 +0400

    prettier

--- docs/registry/god-rays-example.tsx
+++ docs/registry/god-rays-example.tsx
@@ -1,9 +1,5 @@
 import { GodRays, GodRaysProps } from '@paper-design/shaders-react';
 
 export function GodRaysExample(props: GodRaysProps) {
-  return (
-    <GodRays
-      style={{ position: 'fixed', width: '100%', height: '100%' }}
-    />
-  );
+  return <GodRays style={{ position: 'fixed', width: '100%', height: '100%' }} />;
 }
