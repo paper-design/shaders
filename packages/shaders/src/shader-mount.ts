5e2ab12efc6f4796fa3348f119b30cf6ac62d78e
Ksenia Kondrashova <kondrashova.ksenya@gmail.com>
2025-05-06 16:29:45 +0400

    fixing full-size (recalculate object fixed ratio only in auto mode)

--- packages/shaders/src/shader-mount.ts
+++ packages/shaders/src/shader-mount.ts
@@ -565,7 +565,9 @@ void main() {
     (u_worldWidth == 0.) ? u_resolution.x : givenBoxSize.x,
     (u_worldHeight == 0.) ? u_resolution.y : givenBoxSize.y
   );
-  fixedRatio = fixedRatioBoxGivenSize.x / fixedRatioBoxGivenSize.y;
+  if (u_worldWidth == 0. || u_worldHeight == 0.) {
+    fixedRatio = fixedRatioBoxGivenSize.x / fixedRatioBoxGivenSize.y;
+  }
 
   v_objectBoxSize = getBoxSize(fixedRatio, fixedRatioBoxGivenSize, maxBoxSize).xy;
   vec2 objectWorldScale = u_resolution.xy / v_objectBoxSize;
