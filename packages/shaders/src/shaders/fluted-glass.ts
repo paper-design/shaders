85ec0b3db177f1f773f9374322663230e5c21658
Ksenia Kondrashova <kondrashova.ksenya@gmail.com>
2025-06-06 23:43:02 +0200

    frost power, clean up

--- packages/shaders/src/shaders/fluted-glass.ts
+++ packages/shaders/src/shaders/fluted-glass.ts
@@ -36,11 +36,11 @@ ${declarePI}
 ${declareRotate}
 ${declareRandom}
 
+out vec4 fragColor;
+
 vec2 random2(vec2 p) {
   return vec2(random(p), random(200. * p));
 }
-
-out vec4 fragColor;
 
 float uvFrame(vec2 uv) {
   return step(1e-3, uv.x) * step(uv.x, 1. - 1e-3) * step(1e-3, uv.y) * step(uv.y, 1. - 1e-3);
@@ -52,36 +52,36 @@ float hash(float x) {
 
 const int MAX_RADIUS = 25;
 
-vec4 gaussian1D(sampler2D tex, vec2 uv, vec2 texelSize, vec2 dir, float sigma) {
-    if (sigma <= .5) return texture(tex, uv);
-    int radius = int(min(float(MAX_RADIUS), ceil(3.0 * sigma)));
+vec4 getBlur(sampler2D tex, vec2 uv, vec2 texelSize, vec2 dir, float sigma) {
+  if (sigma <= .5) return texture(tex, uv);
+  int radius = int(min(float(MAX_RADIUS), ceil(3.0 * sigma)));
 
-    float twoSigma2 = 2.0 * sigma * sigma;
-    float gaussianNorm = 1.0 / sqrt(TWO_PI * sigma * sigma);
+  float twoSigma2 = 2.0 * sigma * sigma;
+  float gaussianNorm = 1.0 / sqrt(TWO_PI * sigma * sigma);
 
-    vec4 sum        = texture(tex, uv) * gaussianNorm;
-    float weightSum = gaussianNorm;
+  vec4 sum = texture(tex, uv) * gaussianNorm;
+  float weightSum = gaussianNorm;
 
-    for (int i = 1; i <= MAX_RADIUS; ++i) {
-        if (i > radius) break; 
+  for (int i = 1; i <= MAX_RADIUS; i++) {
+    if (i > radius) break;
 
-        float x  = float(i);
-        float w  = exp(-(x * x) / twoSigma2) * gaussianNorm;
+    float x = float(i);
+    float w = exp(-(x * x) / twoSigma2) * gaussianNorm;
 
-        vec2 offset = dir * texelSize * x;
-        vec4 s1 = texture(tex, uv + offset);
-        vec4 s2 = texture(tex, uv - offset);
+    vec2 offset = dir * texelSize * x;
+    vec4 s1 = texture(tex, uv + offset);
+    vec4 s2 = texture(tex, uv - offset);
 
-        sum        += (s1 + s2) * w;
-        weightSum  += 2.0 * w;
-    }
+    sum += (s1 + s2) * w;
+    weightSum += 2.0 * w;
+  }
 
-    return sum / weightSum;
+  return sum / weightSum;
 }
 
 void main() {
   vec2 patternUV = v_patternUV;
-  
+
   vec2 imageUV = v_responsiveUV + .5;
   float screenRatio = v_responsiveBoxGivenSize.x / v_responsiveBoxGivenSize.y;
   float imageRatio = u_image_aspect_ratio;
@@ -99,34 +99,33 @@ void main() {
   vec2 uv = imageUV;
   float frame = uvFrame(imageUV);
   if (frame < .05) discard;
-  
+
   float gridNumber = u_grid * u_image_aspect_ratio;
-  
-  float mask = 
+
+  float mask =
     step(u_marginLeft, imageUV.x) * step(u_marginRight, 1. - imageUV.x)
     * step(u_marginTop, imageUV.y) * step(u_marginBottom, 1. - imageUV.y);
-  
+
   float patternRotation = u_gridRotation * PI / 180.;
   uv = rotate(uv - vec2(.5), patternRotation);
   uv *= gridNumber;
 
   float curveX = 15. * u_curveFreq * uv.y / gridNumber;
-  
+
   float wave = sin(curveX);
   float zigzag = abs(fract(curveX) - .5);
-  
+
   float curve = mix(wave, zigzag, clamp(u_curve, 1., 2.) - 1.);
   curve *= (clamp(u_curve, 0., 1.) * .2 * gridNumber / u_image_aspect_ratio);
-
 
   vec2 uvOrig = uv;
   uv += curve;
 
   vec2 fractUV = fract(uv);
-  vec2 floorUV = floor(uv);  
-  
+  vec2 floorUV = floor(uv);
+
   vec2 fractOrigUV = fract(uvOrig);
-  vec2 floorOrigUV = floor(uvOrig);  
+  vec2 floorOrigUV = floor(uvOrig);
 
   float gridLines = pow(fractUV.x, 14.);
   gridLines *= mask;
@@ -144,29 +143,29 @@ void main() {
     xDistortion += (.5 + u_shift);
     xDistortion -= pow(abs(fractUV.x), .2) * fractUV.x;
   }
-  
+
   xDistortion *= u_distortion;
 
-  uv = (floorOrigUV + fractOrigUV) / gridNumber;  
+  uv = (floorOrigUV + fractOrigUV) / gridNumber;
   uv.x += xDistortion / gridNumber;
-  
+
   uv.y += 1.5 * u_skew * xDistortion / gridNumber;
-  
+
   uv = rotate(uv, -patternRotation) + vec2(.5);
-  
+
   uv = mix(imageUV, uv, mask);
   float blur = mix(0., u_blur, mask);
-  
-  uv += mask * (random2(uv).xy - .5) * .005 * u_frost;
-  
-  vec4 color = gaussian1D(u_image, uv, 1. / u_resolution, vec2(1.0, 0.0), blur);
+
+  uv += mask * (random2(uv).xy - .5) * .015 * u_frost;
+
+  vec4 color = getBlur(u_image, uv, 1. / u_resolution, vec2(0., 1.), blur);
 
   vec3 midColor = texture(u_image, vec2(floorUV.x / gridNumber - .5, .4 + .2 * hash(floorUV.x))).rgb;
   vec3 highlight = mix(midColor, vec3(1.), u_gridLinesBrightness);
   color.rgb = mix(color.rgb, highlight, u_gridLines * gridLines);
 
   float opacity = color.a;
-  fragColor = vec4(color.rgb, opacity); 
+  fragColor = vec4(color.rgb, opacity);
 }
 
 `;
