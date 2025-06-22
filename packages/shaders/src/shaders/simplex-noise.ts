9c96872e25850ee0273b11358dbe759dbec12723
Ksenia Kondrashova <kondrashova.ksenya@gmail.com>
2025-06-22 14:21:44 +0200

    show only snoise

--- packages/shaders/src/shaders/simplex-noise.ts
+++ packages/shaders/src/shaders/simplex-noise.ts
@@ -33,82 +33,43 @@ ${sizingVariablesDeclaration}
 
 out vec4 fragColor;
 
-vec3 mod289(vec3 x) { return x - floor(x * (1.0 / 289.0)) * 289.0; }
-vec2 mod289(vec2 x) { return x - floor(x * (1.0 / 289.0)) * 289.0; }
-vec3 permute(vec3 x) { return mod289(((x*34.0)+1.0)*x); }
+vec3 mod289(vec3 x) {
+  return x - floor(x * (1. / 289.)) * 289.;
+}
 
-//
-// Description : GLSL 2D simplex noise function
-//      Author : Ian McEwan, Ashima Arts
-//  Maintainer : ijm
-//     Lastmod : 20110822 (ijm)
-//     License :
-//  Copyright (C) 2011 Ashima Arts. All rights reserved.
-//  Distributed under the MIT License. See LICENSE file.
-//  https://github.com/ashima/webgl-noise
-//
+vec2 mod289(vec2 x) {
+  return x - floor(x * (1. / 289.)) * 289.;
+}
+
+vec3 permute(vec3 x) {
+  return mod289(((x * 34.) + 1.) * x);
+}
+
 float snoise(vec2 v) {
-
-  // Precompute values for skewed triangular grid
-  const vec4 C = vec4(0.211324865405187,
-  // (3.0-sqrt(3.0))/6.0
-  0.366025403784439,
-  // 0.5*(sqrt(3.0)-1.0)
-  -0.577350269189626,
-  // -1.0 + 2.0 * C.x
-  0.024390243902439);
-  // 1.0 / 41.0
-
-  // First corner (x0)
+  const vec4 C = vec4(.211324865405187, .366025403784439, -.577350269189626, .024390243902439);
   vec2 i  = floor(v + dot(v, C.yy));
-  vec2 x0 = v - i + dot(i, C.xx);
-
-  // Other two corners (x1, x2)
-  vec2 i1 = vec2(0.0);
-  i1 = (x0.x > x0.y)? vec2(1.0, 0.0):vec2(0.0, 1.0);
-  vec2 x1 = x0.xy + C.xx - i1;
-  vec2 x2 = x0.xy + C.zz;
-
-  // Do some permutations to avoid
-  // truncation effects in permutation
+  vec2 x0 = v -   i + dot(i, C.xx);
+  vec2 i1 = (x0.x > x0.y) ? vec2(1., 0.) : vec2(0., 1.);
+  vec4 x12 = x0.xyxy + C.xxzz;
+  x12.xy -= i1;
   i = mod289(i);
-  vec3 p = permute(
-  permute(i.y + vec3(0.0, i1.y, 1.0))
-  + i.x + vec3(0.0, i1.x, 1.0));
-
-  vec3 m = max(0.5 - vec3(
-  dot(x0, x0),
-  dot(x1, x1),
-  dot(x2, x2)
-  ), 0.0);
-
+  vec3 p = permute(permute(i.y + vec3(0., i1.y, 1.)) + i.x + vec3(0., i1.x, 1.));
+  vec3 m = max(0.5 - vec3(dot(x0, x0), dot(x12.xy, x12.xy), dot(x12.zw, x12.zw)), 0.);
   m = m*m;
   m = m*m;
-
-  // Gradients:
-  //  41 pts uniformly over a line, mapped onto a diamond
-  //  The ring size 17*17 = 289 is close to a multiple
-  //      of 41 (41*7 = 287)
-
-  vec3 x = 2.0 * fract(p * C.www) - 1.0;
+  vec3 x = 2. * fract(p * C.www) - 1.;
   vec3 h = abs(x) - 0.5;
   vec3 ox = floor(x + 0.5);
   vec3 a0 = x - ox;
-
-  // Normalise gradients implicitly by scaling m
-  // Approximation of: m *= inversesqrt(a0*a0 + h*h);
-  m *= 1.79284291400159 - 0.85373472095314 * (a0*a0+h*h);
-
-  // Compute final noise value at P
-  vec3 g = vec3(0.0);
+  m *= 1.79284291400159 - .85373472095314 * (a0*a0 + h*h);
+  vec3 g;
   g.x  = a0.x  * x0.x  + h.x  * x0.y;
-  g.yz = a0.yz * vec2(x1.x, x2.x) + h.yz * vec2(x1.y, x2.y);
-  return 130.0 * dot(m, g);
+  g.yz = a0.yz * x12.xz + h.yz * x12.yw;
+  return 130. * dot(m, g);
 }
-
 float getNoise(vec2 uv, float t) {
-  float noise = .5 * sin(5. * uv.x - 5.3 * t);
-  noise += .5 * cos(5. * 2. * uv.y + 8.32 * t);
+  float noise = .5 * snoise(uv - vec2(0., .3 * t));
+  noise += .5 * snoise(2. * uv + vec2(0., .32 * t));
 
   return noise;
 }
@@ -132,49 +93,49 @@ void main() {
 
   float shape = .5 + .5 * getNoise(shape_uv, t);
 
-  bool u_extraSides = true;
+  // bool u_extraSides = true;
+  //
+  // float mixer = shape * (u_colorsCount - 1.);
+  // if (u_extraSides == true) {
+  //   mixer = (shape - .5 / u_colorsCount) * u_colorsCount;
+  // }
+  //
+  // float steps = max(1., u_stepsPerColor);
+  //
+  // vec4 gradient = u_colors[0];
+  // gradient.rgb *= gradient.a;
+  // for (int i = 1; i < ${simplexNoiseMeta.maxColorCount}; i++) {
+  //     if (i >= int(u_colorsCount)) break;
+  //
+  //     float localT = clamp(mixer - float(i - 1), 0., 1.);
+  //     localT = steppedSmooth(localT, steps, u_softness);
+  //
+  //     vec4 c = u_colors[i];
+  //     c.rgb *= c.a;
+  //     gradient = mix(gradient, c, localT);
+  // }
+  //
+  // if (u_extraSides == true) {
+  //  if ((mixer < 0.) || (mixer > (u_colorsCount - 1.))) {
+  //    float localT = mixer + 1.;
+  //    if (mixer > (u_colorsCount - 1.)) {
+  //      localT = mixer - (u_colorsCount - 1.);
+  //    }
+  //    localT = steppedSmooth(localT, steps, u_softness);
+  //    vec4 cFst = u_colors[0];
+  //    cFst.rgb *= cFst.a;
+  //    vec4 cLast = u_colors[int(u_colorsCount - 1.)];
+  //    cLast.rgb *= cLast.a;
+  //    gradient = mix(cLast, cFst, localT);
+  //  }
+  // }
+  //
+  // vec3 color = gradient.rgb;
+  // float opacity = gradient.a;
+  //
+  // $ {colorBandingFix}
 
-  float mixer = shape * (u_colorsCount - 1.);
-  if (u_extraSides == true) {
-    mixer = (shape - .5 / u_colorsCount) * u_colorsCount;
-  }
-
-  float steps = max(1., u_stepsPerColor);
-
-  vec4 gradient = u_colors[0];
-  gradient.rgb *= gradient.a;
-  for (int i = 1; i < ${simplexNoiseMeta.maxColorCount}; i++) {
-      if (i >= int(u_colorsCount)) break;
-
-      float localT = clamp(mixer - float(i - 1), 0., 1.);
-      localT = steppedSmooth(localT, steps, u_softness);
-
-      vec4 c = u_colors[i];
-      c.rgb *= c.a;
-      gradient = mix(gradient, c, localT);
-  }
-
-  if (u_extraSides == true) {
-   if ((mixer < 0.) || (mixer > (u_colorsCount - 1.))) {
-     float localT = mixer + 1.;
-     if (mixer > (u_colorsCount - 1.)) {
-       localT = mixer - (u_colorsCount - 1.);
-     }
-     localT = steppedSmooth(localT, steps, u_softness);
-     vec4 cFst = u_colors[0];
-     cFst.rgb *= cFst.a;
-     vec4 cLast = u_colors[int(u_colorsCount - 1.)];
-     cLast.rgb *= cLast.a;
-     gradient = mix(cLast, cFst, localT);
-   }
-  }
-
-  vec3 color = gradient.rgb;
-  float opacity = gradient.a;
-
-  ${colorBandingFix}
-
-  fragColor = vec4(color, opacity);
+  fragColor = vec4(vec3(shape), 1.);
 }
 `;
 
