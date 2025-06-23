eee4b8a03d42c9478d086e2c44aefbf7ce64b27e
Ksenia Kondrashova <kondrashova.ksenya@gmail.com>
2025-06-23 11:46:59 +0200

    testing selective highp for simplex noise (7)

--- packages/shaders/src/shaders/simplex-noise.ts
+++ packages/shaders/src/shaders/simplex-noise.ts
@@ -35,33 +35,33 @@ out highp vec4 fragColor;
 
 highp vec3 permute(highp vec3 x) { return mod(((x * 34.0) + 1.0) * x, 289.0); }
 highp float snoise(highp vec2 v) {
-  const highp vec4 C = vec4(0.211324865405187, 0.366025403784439,
+  const vec4 C = vec4(0.211324865405187, 0.366025403784439,
   -0.577350269189626, 0.024390243902439);
-  highp vec2 i = floor(v + dot(v, C.yy));
-  highp vec2 x0 = v - i + dot(i, C.xx);
-  highp vec2 i1;
+  vec2 i = floor(v + dot(v, C.yy));
+  vec2 x0 = v - i + dot(i, C.xx);
+  vec2 i1;
   i1 = (x0.x > x0.y) ? vec2(1.0, 0.0) : vec2(0.0, 1.0);
-  highp vec4 x12 = x0.xyxy + C.xxzz;
+  vec4 x12 = x0.xyxy + C.xxzz;
   x12.xy -= i1;
   i = mod(i, 289.0);
-  highp vec3 p = permute(permute(i.y + vec3(0.0, i1.y, 1.0))
+  vec3 p = permute(permute(i.y + vec3(0.0, i1.y, 1.0))
   + i.x + vec3(0.0, i1.x, 1.0));
-  highp vec3 m = max(0.5 - vec3(dot(x0, x0), dot(x12.xy, x12.xy),
+  vec3 m = max(0.5 - vec3(dot(x0, x0), dot(x12.xy, x12.xy),
   dot(x12.zw, x12.zw)), 0.0);
   m = m * m;
   m = m * m;
-  highp vec3 x = 2.0 * fract(p * C.www) - 1.0;
-  highp vec3 h = abs(x) - 0.5;
-  highp vec3 ox = floor(x + 0.5);
-  highp vec3 a0 = x - ox;
+  vec3 x = 2.0 * fract(p * C.www) - 1.0;
+  vec3 h = abs(x) - 0.5;
+  vec3 ox = floor(x + 0.5);
+  vec3 a0 = x - ox;
   m *= 1.79284291400159 - 0.85373472095314 * (a0 * a0 + h * h);
-  highp vec3 g;
+  vec3 g;
   g.x = a0.x * x0.x + h.x * x0.y;
   g.yz = a0.yz * x12.xz + h.yz * x12.yw;
   return 130.0 * dot(m, g);
 }
 
-highp float getNoise(highp vec2 uv, highp float t) {
+highp float getNoise(highp vec2 uv, float t) {
   highp float noise = .5 * snoise(uv - vec2(0., .3 * t));
   noise += .5 * snoise(2. * uv + vec2(0., .32 * t));
 
@@ -82,7 +82,7 @@ void main() {
   highp vec2 shape_uv = v_patternUV;
   shape_uv *= .001;
 
-  highp float t = .2 * u_time;
+  float t = .2 * u_time;
 
   highp float shape = .5 + .5 * getNoise(shape_uv, t);
 
