fd97da3d432de51db52af01a094c09b547376e8d
Ksenia Kondrashova <kondrashova.ksenya@gmail.com>
2025-06-23 11:59:03 +0200

    testing selective highp for simplex noise (10)

--- packages/shaders/src/shaders/simplex-noise.ts
+++ packages/shaders/src/shaders/simplex-noise.ts
@@ -1,7 +1,7 @@
 import type { vec4 } from '../types.js';
 import type { ShaderMotionParams } from '../shader-mount.js';
 import { sizingVariablesDeclaration, type ShaderSizingParams, type ShaderSizingUniforms } from '../shader-sizing.js';
-import { declareSimplexNoise, colorBandingFix } from '../shader-utils.js';
+import { declareSimplexNoiseHighPr, colorBandingFix } from '../shader-utils.js';
 
 export const simplexNoiseMeta = {
   maxColorCount: 10,
@@ -21,7 +21,7 @@ export const simplexNoiseMeta = {
 export const simplexNoiseFragmentShader: string = `#version 300 es
 precision mediump float;
 
-uniform float u_time;
+uniform highp float u_time;
 uniform highp float u_scale;
 
 uniform vec4 u_colors[${simplexNoiseMeta.maxColorCount}];
@@ -33,35 +33,9 @@ ${sizingVariablesDeclaration}
 
 out vec4 fragColor;
 
-highp vec3 permute(highp vec3 x) { return mod(((x * 34.0) + 1.0) * x, 289.0); }
-highp float snoise(highp vec2 v) {
-  const vec4 C = vec4(0.211324865405187, 0.366025403784439,
-  -0.577350269189626, 0.024390243902439);
-  vec2 i = floor(v + dot(v, C.yy));
-  vec2 x0 = v - i + dot(i, C.xx);
-  vec2 i1;
-  i1 = (x0.x > x0.y) ? vec2(1.0, 0.0) : vec2(0.0, 1.0);
-  vec4 x12 = x0.xyxy + C.xxzz;
-  x12.xy -= i1;
-  i = mod(i, 289.0);
-  vec3 p = permute(permute(i.y + vec3(0.0, i1.y, 1.0))
-  + i.x + vec3(0.0, i1.x, 1.0));
-  vec3 m = max(0.5 - vec3(dot(x0, x0), dot(x12.xy, x12.xy),
-  dot(x12.zw, x12.zw)), 0.0);
-  m = m * m;
-  m = m * m;
-  vec3 x = 2.0 * fract(p * C.www) - 1.0;
-  vec3 h = abs(x) - 0.5;
-  vec3 ox = floor(x + 0.5);
-  vec3 a0 = x - ox;
-  m *= 1.79284291400159 - 0.85373472095314 * (a0 * a0 + h * h);
-  vec3 g;
-  g.x = a0.x * x0.x + h.x * x0.y;
-  g.yz = a0.yz * x12.xz + h.yz * x12.yw;
-  return 130.0 * dot(m, g);
-}
+${declareSimplexNoiseHighPr}
 
-highp float getNoise(highp vec2 uv, float t) {
+highp float getNoise(highp vec2 uv, highp float t) {
   float noise = .5 * snoise(uv - vec2(0., .3 * t));
   noise += .5 * snoise(2. * uv + vec2(0., .32 * t));
 
@@ -82,7 +56,7 @@ void main() {
   highp vec2 shape_uv = v_patternUV;
   shape_uv *= .001;
 
-  float t = .2 * u_time;
+  highp float t = .2 * u_time;
 
   highp float shape = .5 + .5 * getNoise(shape_uv, t);
 
@@ -120,7 +94,7 @@ void main() {
   vec3 color = gradient.rgb;
   float opacity = gradient.a;
 
-  //$ {colorBandingFix}
+  ${colorBandingFix}
 
   fragColor = vec4(color, opacity);
 }
