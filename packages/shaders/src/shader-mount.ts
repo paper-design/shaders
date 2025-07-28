82c39ed3a71f1f0874f8b761678d60412bf7808d
Ksenia Kondrashova <kondrashova.ksenya@gmail.com>
2025-07-28 23:26:08 +0400

    don't start the loop when taking preview pics

--- packages/shaders/src/shader-mount.ts
+++ packages/shaders/src/shader-mount.ts
@@ -295,12 +295,12 @@ export class ShaderMount {
       this.saveFirstFrameImage();
     }
 
-    // Loop if we're animating
-    if (this.speed !== 0) {
-      this.requestRender();
-    } else {
-      this.rafId = null;
-    }
+    // // Loop if we're animating
+    // if (this.speed !== 0) {
+    //   this.requestRender();
+    // } else {
+    //   this.rafId = null;
+    // }
   };
 
   private requestRender = () => {
