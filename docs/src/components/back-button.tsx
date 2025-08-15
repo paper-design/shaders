b0e75a08c2d504d433b560aaf3be61fefa41c420
Giel <g.cobben@gmail.com>
2025-08-15 23:14:21 +0200

    fix back button when clicking cmd + click

--- docs/src/components/back-button.tsx
+++ docs/src/components/back-button.tsx
@@ -6,8 +6,17 @@ export function BackButton({ className = '' }: { className?: string }) {
 
   return (
     <button
-      onClick={() => router.back()}
-      className={`${className} bg-background/80 fixed top-3 left-3 z-10 aspect-square rounded-full p-2 shadow-sm dark:outline dark:outline-white/10`}
+      onClick={() => {
+        // go back if there is a history
+        if (typeof window !== 'undefined' && window.history.length > 1) {
+          router.back();
+          return;
+        }
+
+        // otherwise go to home (cmd + click)
+        router.push('/');
+      }}
+      className={`bg-background/80 fixed top-3 left-3 z-10 aspect-square cursor-pointer rounded-full p-2 shadow-sm dark:outline dark:outline-white/10 ${className}`}
     >
       <BackIcon className="size-6" />
     </button>
