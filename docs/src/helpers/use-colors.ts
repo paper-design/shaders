8912c9d9ee44e8a4c8a6bc0139d27bb4b3d4b1e2
Vlad Moroz <hi@vladmoroz.com>
2025-08-25 17:51:04 +0200

    jfc leva

--- docs/src/helpers/use-colors.ts
+++ docs/src/helpers/use-colors.ts
@@ -1,7 +1,7 @@
 import { folder, useControls } from 'leva';
 import { setParamsSafe } from './use-reset-leva-params';
 import { toHsla } from './to-hsla';
-import { useEffect, useState } from 'react';
+import { useEffect, useLayoutEffect, useState } from 'react';
 
 interface UseColorsArgs {
   defaultColors: string[];
@@ -9,7 +9,7 @@ interface UseColorsArgs {
 }
 
 export function useColors({ defaultColors, maxColorCount }: UseColorsArgs) {
-  const [colorsForLeva, setColorsForLeva] = useState(() => {
+  const [presetColors, setPresetColors] = useState(() => {
     return Object.fromEntries(
       defaultColors.map((color: string, index: number) => {
         return [`color${index + 1}`, toHsla(color)];
@@ -17,23 +17,30 @@ export function useColors({ defaultColors, maxColorCount }: UseColorsArgs) {
     );
   });
 
-  const [{ colorCount }, setColorCount] = useControls(() => ({
-    Colors: folder({
-      colorCount: {
-        value: defaultColors.length,
-        min: 1,
-        max: maxColorCount,
-        step: 1,
-      },
-    }),
-  }));
+  const [{ colorCount }, setColorCount] = useControls(() => {
+    return {
+      Colors: folder({
+        colorCount: {
+          value: Object.values(presetColors).length,
+          min: 1,
+          max: maxColorCount,
+          step: 1,
+        },
+      }),
+    };
+  });
+
+  useLayoutEffect(() => {
+    setColorCount({ colorCount: Object.values(presetColors).length });
+  }, [presetColors, setColorCount]);
 
   const [levaColors, setLevaColors] = useControls(() => {
+    console.log('useControls: leva colors');
     const colors: Record<string, { value: string }> = {};
 
     for (let i = 0; i < colorCount; i++) {
       colors[`color${i + 1}`] = {
-        value: defaultColors[i] ? toHsla(defaultColors[i]) : `hsla(${(40 * i) % 360}, 60%, 50%, 1)`,
+        value: presetColors[i] ? toHsla(presetColors[i]) : `hsla(${(40 * i) % 360}, 60%, 50%, 1)`,
       };
     }
 
@@ -42,32 +49,20 @@ export function useColors({ defaultColors, maxColorCount }: UseColorsArgs) {
     };
   }, [colorCount]);
 
-  // Update params after colorsForLeva changes (timing issues)
   useEffect(() => {
-    setParamsSafe(colorsForLeva, setLevaColors, colorsForLeva);
-  }, [colorsForLeva, setLevaColors]);
-
-  // Reset colors to defaults when component mounts
-  useEffect(() => {
-    const defaultColorValues = Object.fromEntries(
-      defaultColors.map((color: string, index: number) => {
-        return [`color${index + 1}`, toHsla(color)];
-      })
-    );
-
-    setColorCount({ colorCount: defaultColors.length });
-    setParamsSafe(levaColors, setLevaColors, defaultColorValues);
-  }, []);
+    if (Object.values(presetColors).length === colorCount) {
+      setParamsSafe(presetColors, setLevaColors, presetColors);
+    }
+  }, [presetColors, colorCount, setLevaColors]);
 
   const setColors = (colors: string[]) => {
-    const colorsAsObj = Object.fromEntries(
-      colors.map((color: string, index: number) => {
-        return [`color${index + 1}`, toHsla(color)];
-      })
+    setPresetColors(
+      Object.fromEntries(
+        colors.map((color: string, index: number) => {
+          return [`color${index + 1}`, toHsla(color)];
+        })
+      )
     );
-
-    setColorsForLeva(colorsAsObj);
-    setColorCount({ colorCount: colors.length });
   };
 
   const colors = Object.values(levaColors) as unknown as string[];
