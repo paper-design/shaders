bec17032d79fa526696b7485d7e09061991996e0
Ksenia Kondrashova <kondrashova.ksenya@gmail.com>
2025-08-25 18:45:32 +0400

    test3

--- docs/src/components/shader-container.tsx
+++ docs/src/components/shader-container.tsx
@@ -1,3 +1,3 @@
 export function ShaderContainer({ children }: { children: React.ReactNode }) {
-  return <div className="h-[20rem] overflow-hidden rounded-3xl sm:h-[36rem] [&>div]:size-full">{children}</div>;
+  return <div className="h-[20rem] overflow-hidden rounded-3xl sm:h-[80rem] [&>div]:size-full">{children}</div>;
 }
