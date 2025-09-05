4cdba19d899dd79de40423774bfa59f55432a497
Michael Dougall <6801309+itsdouges@users.noreply.github.com>
2025-09-05 13:56:52 +1000

    chore: use preset

--- docs/src/app/test/page.tsx
+++ docs/src/app/test/page.tsx
@@ -1,13 +1,12 @@
-import { Water, Heatmap } from '@paper-design/shaders-react';
+'use client';
+
+import { Water, Heatmap, heatmapPresets } from '@paper-design/shaders-react';
 
 export default function TestPage() {
   return (
     <div style={{ display: 'flex' }}>
       <Water style={{ width: 600, height: 600 }} />
-      <Heatmap
-        image="https://workers.paper-staging.dev/file-assets/01K44QHJ96H7WP54KYYY9VPJHJ/01K4BPJWG3CVFWG7NSR3E7Q0EE.svg"
-        style={{ width: 600, height: 600 }}
-      />
+      <Heatmap image={heatmapPresets[0].params.image} style={{ width: 600, height: 600 }} />
     </div>
   );
 }
