26c9c98fd05212ca2fc7b9c4cda7f1d33398ebfb
Ksenia Kondrashova <kondrashova.ksenya@gmail.com>
2025-09-08 14:19:16 +0400

    temporary logos to test

--- packages/shaders-react/src/shaders/heatmap.tsx
+++ packages/shaders-react/src/shaders/heatmap.tsx
@@ -22,7 +22,7 @@ export const defaultPreset: HeatmapPreset = {
   name: 'Default',
   params: {
     ...defaultObjectSizing,
-    image: 'https://workers.paper-staging.dev/file-assets/01K44QHJ96H7WP54KYYY9VPJHJ/01K4BPJWG3CVFWG7NSR3E7Q0EE.svg',
+    image: './heatmap-temporary/logo-pics/apple.svg',
     contour: 0.5,
     angle: 0,
     proportion: 0.5,
