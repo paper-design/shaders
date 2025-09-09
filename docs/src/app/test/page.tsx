7be9bae65b34beb76f0c77e18095e7d9ecdb82e3
Michael Dougall <6801309+itsdouges@users.noreply.github.com>
2025-09-09 21:05:28 +1000

    feat: add default preset image

--- docs/src/app/test/page.tsx
+++ docs/src/app/test/page.tsx
@@ -1,17 +1,13 @@
 'use client';
 
-import { Water, Heatmap, heatmapPresets } from '@paper-design/shaders-react';
+import { Heatmap, heatmapPresets } from '@paper-design/shaders-react';
 
 export default function TestPage() {
   return (
     <div style={{ display: 'flex' }}>
       <Heatmap
-          // colors={['#ffffff', '#ff0000', '#ffff00']}
-          // colorBack={'#000000'}
-          // noise={0.5}
-          // speed={2}
-          image={'./heatmap-temporary/logo-pics/apple.svg'}
-          style={{ width: 600, height: 600 }}
+        {...heatmapPresets.find((preset) => preset.name === 'Default')?.params}
+        style={{ width: 600, height: 600 }}
       />
     </div>
   );
