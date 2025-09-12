9f1066187cd01a044a1e9a32ad3f6df8cf7dc573
Dalim <contact@dalim.in>
2025-09-13 01:46:49 +0530

    Update layout.tsx

--- docs/src/app/(shaders)/line-grid/layout.tsx
+++ docs/src/app/(shaders)/line-grid/layout.tsx
@@ -1,7 +1,7 @@
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: 'Ripple • Paper',
+  title: 'Line Grid • Paper',
 };
 
 export default function Layout({ children }: { children: React.ReactNode }) {
