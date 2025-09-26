ee4e3f832e0176566462667941f35ebe9bc6150f
verekia <522007+verekia@users.noreply.github.com>
2025-09-26 23:20:43 +0200

    docs: update image liquid metal to always use an image, cleanups

--- docs/src/app/(shaders)/image-liquid-metal/page.tsx
+++ docs/src/app/(shaders)/image-liquid-metal/page.tsx
@@ -5,12 +5,10 @@ import { useControls, button, folder } from 'leva';
 import { setParamsSafe, useResetLevaParams } from '@/helpers/use-reset-leva-params';
 import { usePresetHighlight } from '@/helpers/use-preset-highlight';
 import { cleanUpLevaParams } from '@/helpers/clean-up-leva-params';
-import { DitheringType, DitheringTypes, ShaderFit } from '@paper-design/shaders';
+import { ShaderFit } from '@paper-design/shaders';
 import { levaImageButton } from '@/helpers/leva-image-button';
-import { useState, useEffect, useCallback, Suspense } from 'react';
-import { toHsla } from '@/helpers/color-utils';
+import { useState, Suspense } from 'react';
 import { ShaderDetails } from '@/components/shader-details';
-import { imageDitheringDef } from '@/shader-defs/image-dithering-def';
 import { ShaderContainer } from '@/components/shader-container';
 import { useUrlParams } from '@/helpers/use-url-params';
 import { imageLiquidMetalDef } from '@/shader-defs/image-liquid-metal-def';
@@ -18,17 +16,7 @@ import { imageLiquidMetalDef } from '@/shader-defs/image-liquid-metal-def';
 const { worldWidth, worldHeight, ...defaults } = imageLiquidMetalPresets[0].params;
 
 const ImageLiquidMetalWithControls = () => {
-  const [image, setImage] = useState<HTMLImageElement | undefined>(undefined);
-
-  useEffect(() => {
-    const img = new Image();
-    img.src = `/images/image-filters/0019.webp`;
-    img.onload = () => setImage(img);
-  }, []);
-
-  const setImageWithoutStatus = useCallback((img?: HTMLImageElement) => {
-    setImage(img);
-  }, []);
+  const [image, setImage] = useState<HTMLImageElement | string>('/images/image-filters/0019.webp');
 
   const [params, setParams] = useControls(() => {
     const presets = Object.fromEntries(
@@ -49,12 +37,9 @@ const ImageLiquidMetalWithControls = () => {
       offsetX: { value: defaults.offsetX, min: -1, max: 1, order: 303 },
       offsetY: { value: defaults.offsetY, min: -1, max: 1, order: 304 },
       fit: { value: defaults.fit, options: ['contain', 'cover'] as ShaderFit[], order: 305 },
-      Image: folder(
-        {
-          'Upload image': levaImageButton(setImageWithoutStatus),
-        },
-        { order: 0 }
-      ),
+      Image: folder({
+        'Upload image': levaImageButton((img?: HTMLImageElement) => setImage(img ?? '')),
+      }),
       Presets: folder(presets, { order: -1 }),
     };
   });
@@ -70,7 +55,7 @@ const ImageLiquidMetalWithControls = () => {
     <>
       <ShaderContainer shaderDef={imageLiquidMetalDef} currentParams={params}>
         <Suspense fallback={null}>
-          <ImageLiquidMetal {...params} image={image || undefined} suspendWhenProcessingImage />
+          <ImageLiquidMetal {...params} image={image} suspendWhenProcessingImage />
         </Suspense>
       </ShaderContainer>
       <ShaderDetails shaderDef={imageLiquidMetalDef} currentParams={params} codeSampleImageName="diamond.webp" />
