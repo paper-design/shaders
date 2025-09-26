aede5e2a983a3cdac5fd1dae92953d3ef004e11d
Vlad Moroz <hi@vladmoroz.com>
2025-09-26 16:35:54 +0200

    test page

--- docs/src/app/test/page.tsx
+++ docs/src/app/test/page.tsx
@@ -4,12 +4,12 @@ import { FlutedGlass, Heatmap, ImageDithering, PaperTexture, Water } from '@pape
 
 export default function TestPage() {
   return (
-    <div className="grid grid-cols-3">
-      <FlutedGlass height={600} image="" />
-      <PaperTexture height={600} />
-      <Water height={600} />
-      <ImageDithering height={600} image="" />
-      <Heatmap height={600} image="" />
+    <div className="grid grid-cols-3 *:aspect-video">
+      <FlutedGlass image="" />
+      <PaperTexture />
+      <Water />
+      <ImageDithering image="" />
+      <Heatmap image="" />
     </div>
   );
 }
