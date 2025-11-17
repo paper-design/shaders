69b9b0bd8da4da30936c9be78e5b435654e73ae5
Ksenia Kondrashova <kondrashova.ksenya@gmail.com>
2025-11-17 18:28:54 +0100

    halftoneLines classic preset

--- packages/shaders-react/src/shaders/halftone-lines.tsx
+++ packages/shaders-react/src/shaders/halftone-lines.tsx
@@ -35,7 +35,31 @@ export const defaultPreset: HalftoneLinesPreset = {
     grainOverlay: 0.2,
   },
 };
-export const halftoneLinesPresets: HalftoneLinesPreset[] = [defaultPreset];
+
+export const classicPreset: HalftoneLinesPreset = {
+  name: 'Classic',
+  params: {
+    ...defaultObjectSizing,
+    scale: 1,
+    speed: 0,
+    frame: 0,
+    colorBack: '#ffffff',
+    colorFront: '#000000',
+    stripeWidth: 1,
+    smoothness: 10,
+    size: 60,
+    angleDistortion: 0,
+    noiseDistortion: 0,
+    angle: 0,
+    contrast: 0.7,
+    originalColors: false,
+    inverted: false,
+    grainMixer: 0,
+    grainOverlay: 0,
+  },
+};
+
+export const halftoneLinesPresets: HalftoneLinesPreset[] = [defaultPreset, classicPreset];
 
 export const HalftoneLines: React.FC<HalftoneLinesProps> = memo(function HalftoneLinesImpl({
   // Own props
