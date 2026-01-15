7085e1685230bef7718b4c3e8b545dfc4e6a14fe
Ksenia Kondrashova <kondrashova.ksenya@gmail.com>
2026-01-15 15:08:00 +0400

    HalftoneCmyk alpha support (#247)

--- docs/src/app/(shaders)/halftone-cmyk/page.tsx
+++ docs/src/app/(shaders)/halftone-cmyk/page.tsx
@@ -100,7 +100,7 @@ const HalftoneCmykWithControls = () => {
       // originX: { value: defaults.originX, min: 0, max: 1, order: 411 },
       // originY: { value: defaults.originY, min: 0, max: 1, order: 412 },
       // rotation: { value: defaults.rotation, min: 0, max: 360, order: 420 },
-      scale: { value: defaults.scale, min: 0, max: 10, order: 420 },
+      scale: { value: defaults.scale, min: 0, max: 4, order: 420 },
       fit: { value: defaults.fit, options: ['contain', 'cover'] as ShaderFit[], order: 450 },
       Image: folder(
         {
