daa20f1cf871576b51b04c00590dd28f390f650e
Ksenia Kondrashova <kondrashova.ksenya@gmail.com>
2026-02-10 02:14:04 +0100

    empty pixel to string

--- packages/shaders/src/get-empty-pixel.ts
+++ packages/shaders/src/get-empty-pixel.ts
@@ -1,12 +1,1 @@
-export function getEmptyPixel(): HTMLImageElement | undefined {
-  if (typeof window === 'undefined') {
-    console.warn('Paper Shaders: can’t create an image on the server');
-    return undefined;
-  }
-
-  const img = new Image();
-  img.src = emptyPixel;
-  return img;
-}
-
-const emptyPixel = 'data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==';
+export const emptyPixel = 'data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==';
