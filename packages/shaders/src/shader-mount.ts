75b09ecca1ffd64ceba99e3ddfdd5e644c9faf4b
ksenia <kondrashova.ksenya@gmail.com>
2026-07-15 15:42:33 +0400

    clean up: updateCurrentSpeed method

--- packages/shaders/src/shader-mount.ts
+++ packages/shaders/src/shader-mount.ts
@@ -18,7 +18,7 @@ export class ShaderMount {
   private currentFrame = 0;
   /** The speed that we progress through animation time (multiplies by delta time every update). Allows negatives to play in reverse. If set to 0, rAF will stop entirely so static shaders have no recurring performance costs */
   private speed = 0;
-  /** Actual speed used that accounts for document visibility (we pause the shader if the tab is hidden) */
+  /** Actual speed used that accounts for visibility: we pause the shader if the tab is hidden or the element out of the viewport */
   private currentSpeed = 0;
   /** Uniforms that are provided by the user for the specific shader being mounted (not including uniforms that this Mount adds, like time and resolution) */
   private providedUniforms: ShaderMountUniforms;
@@ -207,7 +207,7 @@ export class ShaderMount {
 
     this.intersectionObserver = new view.IntersectionObserver(([entry]) => {
       this.isInViewport = entry?.isIntersecting ?? true;
-      this.setCurrentSpeed(this.ownerDocument.hidden || !this.isInViewport ? 0 : this.speed);
+      this.updateCurrentSpeed();
     });
 
     this.intersectionObserver.observe(this.parentElement);
@@ -509,7 +509,12 @@ export class ShaderMount {
   public setSpeed = (newSpeed = 1): void => {
     // Set the new animation speed
     this.speed = newSpeed;
-    this.setCurrentSpeed(this.ownerDocument.hidden || !this.isInViewport ? 0 : newSpeed);
+    this.updateCurrentSpeed();
+  };
+
+  /** Apply the target speed, pausing (0) while the tab is hidden or the element is out of the viewport */
+  private updateCurrentSpeed = (): void => {
+    this.setCurrentSpeed(this.ownerDocument.hidden || !this.isInViewport ? 0 : this.speed);
   };
 
   private setCurrentSpeed = (newSpeed: number): void => {
@@ -551,7 +556,7 @@ export class ShaderMount {
   };
 
   private handleDocumentVisibilityChange = () => {
-    this.setCurrentSpeed(this.ownerDocument.hidden || !this.isInViewport ? 0 : this.speed);
+    this.updateCurrentSpeed();
   };
 
   /** Dispose of the shader mount, cleaning up all of the WebGL resources */
