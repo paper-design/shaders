127ffab1d6f365e9bbd817a768713ab11908042c
ksenia <kondrashova.ksenya@gmail.com>
2026-08-13 22:19:25 +0400

    logo image defs

--- docs/src/shader-defs/gem-smoke-def.ts
+++ docs/src/shader-defs/gem-smoke-def.ts
@@ -11,8 +11,7 @@ export const gemSmokeDef: ShaderDef = {
     {
       name: 'image',
       type: 'HTMLImageElement | string',
-      description:
-        'An optional image used as an effect mask. A transparent background is required. If no image is provided, the shader defaults to one of the predefined shapes.',
+      description: 'An optional image used as an effect mask. A transparent background is required.',
     },
     {
       name: 'shape',
